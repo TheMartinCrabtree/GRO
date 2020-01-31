c6c930b96c58ada7e4dcd0276ba1175dc8fe8595
Martin Crabtree <mcrabtr33@gmail.com>
2020-01-31 16:39:12 -0500

    added reducer

---
+++ src/reducer.js
@@ -0,0 +1,26 @@
+const Reducer=(state, action)=>{
+    switch(action.type){
+        case 'SET_ALL_ROBOTS':
+            return{
+                ...state,
+                allrobots: action.payload
+            };
+        case 'ADD_ROBOT':
+            return{
+                ...state,
+                robotHanger: state.robotHanger.push(action.payload)
+            };
+        case 'REMOVE_ROBOT':
+            return{
+                ...state,
+                robotHanger: state.robotHanger.filter((robot)=>{
+                    robot.id !== action.payload
+                })
+            };
+        default:
+            return state;
+    }
+
+}
+
+export default Reducer;
