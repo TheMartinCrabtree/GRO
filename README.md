93105bc856a96d52394535634dc4b93d347e94cd
MC-Games\MC <martin.crabtree@gmail.com>
2020-02-02 20:07:57 -0500

    HUD components

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+
+
+Instructions: Download/Clone and install with "npm install."
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ src/components/MainHud.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const MainHud=(props)=>{
+
+
+    return(
+        <article>
+            <div>
+                <h4>Details:</h4>
+            </div>
+            <div>
+                <h4>Current Team</h4>
+            </div>
+        </article>
+    );
+}
+
+export default MainHud;
